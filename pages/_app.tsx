ac8ffb451aa48bbc06658795af8e526ee820a8ed
Suphachai Phetthamrong <suphachaiphetthamrong@gmail.com>
2022-08-13 21:57:58 +0700

    Add theam

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,25 @@
-import { Container, NextUIProvider } from "@nextui-org/react";
+import { Container, NextUIProvider, createTheme } from "@nextui-org/react";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const theme = createTheme({
+    type: "light",
+    theme: {
+      colors: {
+        gold: "#FFD700",
+        gold1: "#F9F295",
+        gold2: "#E0AA3E",
+        gold3: "#E0AA3E",
+        gold4: "#B88A44",
+        gradient:
+          "linear-gradient(to right, #BF953F, #FCF6BA, #B38728, #FBF5B7, #AA771C)",
+        gradientBackGround: `radial-gradient(ellipse farthest-corner at right bottom, #FEDB37 0%, #FDB931 8%, #9f7928 30%, #8A6E2F 40%, transparent 80%),
+        radial-gradient(ellipse farthest-corner at left top, #FFFFFF 0%, #FFFFAC 8%, #D1B464 25%, #5d4a1f 62.5%, #5d4a1f 100%)`,
+      },
+    },
+  });
   return (
-    <NextUIProvider>
+    <NextUIProvider theme={theme}>
       <Container fluid>
         <Component {...pageProps} />
       </Container>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Card, Grid, Text } from "@nextui-org/react";
+import { Card, Grid, Text, Button } from "@nextui-org/react";
 import type { NextPage } from "next";
 
 type Props = {
@@ -13,19 +13,22 @@ const Home: NextPage = () => {
           <Text h6 size={15} color="white" css={{ mt: 0 }}>
             {text}
           </Text>
+          <Button color="gradient" auto>
+            Gradient
+          </Button>
         </Card.Body>
       </Card>
     );
   };
   return (
-    <Grid.Container gap={2} justify="center">
-      <Grid xs={4}>
+    <Grid.Container gap={1} justify="center">
+      <Grid xs={0} md={3}>
         <MockItem text="1 of 3" />
       </Grid>
-      <Grid xs={4}>
+      <Grid xs={12} md={6}>
         <MockItem text="2 of 3" />
       </Grid>
-      <Grid xs={4}>
+      <Grid xs={0} md={3}>
         <MockItem text="3 of 3" />
       </Grid>
     </Grid.Container>
